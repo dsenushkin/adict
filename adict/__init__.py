b8d633efa61777076bcc84e47b2c53c14206b645
senushkin <senushkin@airi.net>
2024-04-01 09:13:45 +0300

    initial

---
+++ adict/__init__.py
@@ -0,0 +1,51 @@
+from typing import Any
+
+
+class adict(dict):
+    """
+    Simple dot access dictionary. Supports recursion.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(adict, self).__init__()
+
+        for __arg in args:
+            if not isinstance(__arg, dict):
+                raise AttributeError(__arg)
+            for __name, __value in __arg.items():
+                if isinstance(__value, dict):
+                    self.__setattr__(__name, self.__class__(__value))
+                self.__setattr__(__name, __value)
+
+        for __name, __value in kwargs.items():
+            if isinstance(__value, dict):
+                self.__setattr__(__name, self.__class__(__value))
+            self.__setattr__(__name, __value)
+
+    def __getattr__(self, __name: str) -> Any:
+        try:
+            return self[__name]
+        except KeyError:
+            # raise AttributeError(__name)
+            return None
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if isinstance(__value, dict):
+            self[__name] = self.__class__(__value)
+        else:
+            self[__name] = __value
+
+    def __delattr__(self, __name: str) -> None:
+        if __name in self:
+            del self[__name]
+
+    def __getstate__(self):
+        return dict(self.__dict__)
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(%s)" % ", ".join([
+            f"{__name}={__value}" for __name, __value in self.items()
+        ])
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adict",
+    description="Python Attribute Dict that provides dot access.",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Dmitry Senushkin",
+    license="MIT",
+    keywords="adict, dot access dict"
+)
